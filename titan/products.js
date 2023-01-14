ad180c6df90ff214366c65e913d1b0f5c555fc78
hamman20009 <103840054+hamman20009@users.noreply.github.com>
2023-01-14 23:33:08 +0100

    Update products.js

--- titan/products.js
+++ titan/products.js
@@ -2,13 +2,13 @@ const products = [
 	{
 		id: 2,
 		logo:'img/logo.png',
-		image: 'img/products/chol.jpg',
-		image2: 'img/products/chol.jpg',
+		image: 'img/products/titan.JPG',
+		image2: 'img/products/landing.JPG',
 		
 		title: ' TITAN GEL   ',
 		price1: 15500,
 		price: 6900,
-		description: '  السيروم المكون من 7 زيوت الذي يقضي على الصلع و تساقط الشعر و الشيب المبكر '   ,
+		description: '   TITAN GEL ORIGINAL '   ,
 		phone: 0,
 	},
 
